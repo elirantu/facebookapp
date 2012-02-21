8d291780265ddadd92fe462fafc996ad7f9144f6
Eliran Turgeman <eliran.turgeman@klarna.com>
2012-02-21 20:42:42 +0200

    generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebookapp::Application.config.secret_token = '6d0f877cff69ed8b61f9e04020fee5204dcc033d2c37edce5d420d6adbb737e0b22859f97c0e7e822ecbef7da5d4c73ca751c9d1a26896f641eefb0527cfe170'
